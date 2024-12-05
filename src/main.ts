6a61724407b2ffb4708877c4da90bae310685b32
Karoun Kasraie <contact@karoun.me>
2024-12-05 01:52:24 +0000

    Adding app-name-matcher to Action params

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,13 @@ interface ExecResult {
   stderr: string;
 }
 
-interface App {
+interface Diff {
+  app: App;
+  diff: string;
+  error?: ExecResult;
+}
+
+export interface App {
   metadata: { name: string };
   spec: {
     source: {
@@ -29,123 +35,131 @@ interface App {
     };
   };
 }
-const ARCH = process.env.ARCH || 'linux';
-const githubToken = core.getInput('github-token');
-core.info(githubToken);
 
-const ARGOCD_SERVER_URL = core.getInput('argocd-server-url');
-const ARGOCD_TOKEN = core.getInput('argocd-token');
-const VERSION = core.getInput('argocd-version');
-const ENV = core.getInput('environment');
-const PLAINTEXT = core.getInput('plaintext').toLowerCase() === 'true';
-let EXTRA_CLI_ARGS = core.getInput('argocd-extra-cli-args');
-if (PLAINTEXT) {
-  EXTRA_CLI_ARGS += ' --plaintext';
-}
-
-const octokit = github.getOctokit(githubToken);
-
-function execCommand(command: string, options: ExecOptions = {}): Promise<ExecResult> {
-  return new Promise<ExecResult>((done, failed) => {
-    exec(command, options, (err: ExecException | null, stdout: string, stderr: string): void => {
-      const res: ExecResult = {
-        stdout,
-        stderr
-      };
-      if (err) {
-        res.err = err;
-        failed(res);
-        return;
-      }
-      done(res);
-    });
-  });
-}
-
-function scrubSecrets(input: string): string {
-  let output = input;
-  const authTokenMatches = input.match(/--auth-token=([\w.\S]+)/);
-  if (authTokenMatches) {
-    output = output.replace(new RegExp(authTokenMatches[1], 'g'), '***');
+export function filterAppsByName(appsAffected: App[], appNameMatcher: string): App[] {
+  if (appNameMatcher.startsWith('/') && appNameMatcher.endsWith('/')) {
+    const appNameFilter = new RegExp(appNameMatcher.slice(1, -1));
+    return appsAffected.filter(app => appNameFilter.test(app.metadata.name));
+  } else if (appNameMatcher !== '') {
+    const appNames = new Set(appNameMatcher.split(','));
+    return appsAffected.filter(app => appNames.has(app.metadata.name));
   }
-  return output;
-}
-
-async function setupArgoCDCommand(): Promise<(params: string) => Promise<ExecResult>> {
-  const argoBinaryPath = await tc.downloadTool(
-    `https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-${ARCH}-amd64`
-  );
-  fs.chmodSync(argoBinaryPath, '755');
-
-  return async (params: string) =>
-    execCommand(
-      `${argoBinaryPath} ${params} --auth-token=${ARGOCD_TOKEN} --server=${ARGOCD_SERVER_URL} ${EXTRA_CLI_ARGS}`
-    );
-}
-
-async function getApps(): Promise<App[]> {
-  const protocol = PLAINTEXT ? 'http' : 'https';
-  const url = `${protocol}://${ARGOCD_SERVER_URL}/api/v1/applications`;
-  core.info(`Fetching apps from: ${url}`);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  let responseJson: any;
-  try {
-    const response = await nodeFetch(url, {
-      method: 'GET',
-      headers: { Cookie: `argocd.token=${ARGOCD_TOKEN}` }
-    });
-    responseJson = await response.json();
-  } catch (e) {
-    if (e instanceof Error || typeof e === 'string') {
-      core.error(e);
-    }
-  }
-  const apps = responseJson.items as App[];
-  const repoApps = apps.filter(app => {
-    const targetRevision = app.spec.source.targetRevision;
-    const targetPrimary =
-      targetRevision === 'master' || targetRevision === 'main' || !targetRevision;
-    return (
-      app.spec.source.repoURL.includes(
-        `${github.context.repo.owner}/${github.context.repo.repo}`
-      ) && targetPrimary
-    );
-  });
-
-  const changedFiles = await getChangedFiles();
-  core.info(`Changed files: ${changedFiles.join(', ')}`);
-  const appsAffected = repoApps.filter(app => {
-    return partOfApp(changedFiles, app);
-  });
   return appsAffected;
 }
 
-interface Diff {
-  app: App;
-  diff: string;
-  error?: ExecResult;
-}
-async function postDiffComment(diffs: Diff[]): Promise<void> {
-  const protocol = PLAINTEXT ? 'http' : 'https';
-  const { owner, repo } = github.context.repo;
-  const sha = github.context.payload.pull_request?.head?.sha;
+export async function run(): Promise<void> {
+  const ARCH = process.env.ARCH || 'linux';
+  const githubToken = core.getInput('github-token');
+  core.info(githubToken);
 
-  const commitLink = `https://github.com/${owner}/${repo}/pull/${github.context.issue.number}/commits/${sha}`;
-  const shortCommitSha = String(sha).slice(0, 7);
+  const ARGOCD_SERVER_URL = core.getInput('argocd-server-url');
+  const ARGOCD_TOKEN = core.getInput('argocd-token');
+  const VERSION = core.getInput('argocd-version');
+  const ENV = core.getInput('environment');
+  const PLAINTEXT = core.getInput('plaintext').toLowerCase() === 'true';
+  const APP_NAME_MATCHER = core.getInput('app-name-matcher');
+  let EXTRA_CLI_ARGS = core.getInput('argocd-extra-cli-args');
+  if (PLAINTEXT) {
+    EXTRA_CLI_ARGS += ' --plaintext';
+  }
 
-  const filteredDiffs = diffs
-    .map(diff => {
-      diff.diff = filterDiff(diff.diff);
-      return diff;
-    })
-    .filter(d => d.diff !== '');
+  const octokit = github.getOctokit(githubToken);
 
-  const prefixHeader = `## ArgoCD Diff on ${ENV}`;
-  const diffOutput = filteredDiffs.map(
-    ({ app, diff, error }) => `
+  function execCommand(command: string, options: ExecOptions = {}): Promise<ExecResult> {
+    return new Promise<ExecResult>((done, failed) => {
+      exec(command, options, (err: ExecException | null, stdout: string, stderr: string): void => {
+        const res: ExecResult = {
+          stdout,
+          stderr
+        };
+        if (err) {
+          res.err = err;
+          failed(res);
+          return;
+        }
+        done(res);
+      });
+    });
+  }
+
+  function scrubSecrets(input: string): string {
+    let output = input;
+    const authTokenMatches = input.match(/--auth-token=([\w.\S]+)/);
+    if (authTokenMatches) {
+      output = output.replace(new RegExp(authTokenMatches[1], 'g'), '***');
+    }
+    return output;
+  }
+
+  async function setupArgoCDCommand(): Promise<(params: string) => Promise<ExecResult>> {
+    const argoBinaryPath = await tc.downloadTool(
+      `https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-${ARCH}-amd64`
+    );
+    fs.chmodSync(argoBinaryPath, '755');
+
+    return async (params: string) =>
+      execCommand(
+        `${argoBinaryPath} ${params} --auth-token=${ARGOCD_TOKEN} --server=${ARGOCD_SERVER_URL} ${EXTRA_CLI_ARGS}`
+      );
+  }
+
+  async function getApps(): Promise<App[]> {
+    const protocol = PLAINTEXT ? 'http' : 'https';
+    const url = `${protocol}://${ARGOCD_SERVER_URL}/api/v1/applications`;
+    core.info(`Fetching apps from: ${url}`);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let responseJson: any;
+    try {
+      const response = await nodeFetch(url, {
+        method: 'GET',
+        headers: { Cookie: `argocd.token=${ARGOCD_TOKEN}` }
+      });
+      responseJson = await response.json();
+    } catch (e) {
+      if (e instanceof Error || typeof e === 'string') {
+        core.setFailed(e);
+      }
+      return [];
+    }
+    const apps = responseJson.items as App[];
+    const repoApps = apps.filter(app => {
+      const targetRevision = app.spec.source.targetRevision;
+      const targetPrimary =
+        targetRevision === 'master' || targetRevision === 'main' || !targetRevision;
+      return (
+        app.spec.source.repoURL.includes(
+          `${github.context.repo.owner}/${github.context.repo.repo}`
+        ) && targetPrimary
+      );
+    });
+
+    const changedFiles = await getChangedFiles();
+    core.info(`Changed files: ${changedFiles.join(', ')}`);
+    const appsAffected = repoApps.filter(partOfApp.bind(null, changedFiles));
+    return filterAppsByName(appsAffected, APP_NAME_MATCHER);
+  }
+
+  async function postDiffComment(diffs: Diff[]): Promise<void> {
+    const protocol = PLAINTEXT ? 'http' : 'https';
+    const { owner, repo } = github.context.repo;
+    const sha = github.context.payload.pull_request?.head?.sha;
+
+    const commitLink = `https://github.com/${owner}/${repo}/pull/${github.context.issue.number}/commits/${sha}`;
+    const shortCommitSha = String(sha).slice(0, 7);
+
+    const filteredDiffs = diffs
+      .map(diff => {
+        diff.diff = filterDiff(diff.diff);
+        return diff;
+      })
+      .filter(d => d.diff !== '');
+
+    const prefixHeader = `## ArgoCD Diff on ${ENV}`;
+    const diffOutput = filteredDiffs.map(
+      ({ app, diff, error }) => `
 App: [\`${app.metadata.name}\`](${protocol}://${ARGOCD_SERVER_URL}/applications/${
-      app.metadata.name
-    })
+        app.metadata.name
+      })
 YAML generation: ${error ? ' Error 🛑' : 'Success 🟢'}
 App sync status: ${app.status.sync.status === 'Synced' ? 'Synced ✅' : 'Out of Sync ⚠️ '}
 ${
@@ -177,9 +191,9 @@ ${diff}
 }
 ---
 `
-  );
+    );
 
-  const output = scrubSecrets(`
+    const output = scrubSecrets(`
 ${prefixHeader} for commit [\`${shortCommitSha}\`](${commitLink})
 _Updated at ${new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' })} PT_
   ${diffOutput.join('\n')}
@@ -191,78 +205,76 @@ _Updated at ${new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angele
 | 🛑     | There was an error generating the ArgoCD diffs due to changes in this PR. |
 `);
 
-  const commentsResponse = await octokit.rest.issues.listComments({
-    issue_number: github.context.issue.number,
-    owner,
-    repo
-  });
+    const commentsResponse = await octokit.rest.issues.listComments({
+      issue_number: github.context.issue.number,
+      owner,
+      repo
+    });
 
-  // Delete stale comments
-  for (const comment of commentsResponse.data) {
-    if (comment.body?.includes(prefixHeader)) {
-      core.info(`deleting comment ${comment.id}`);
-      octokit.rest.issues.deleteComment({
+    // Delete stale comments
+    for (const comment of commentsResponse.data) {
+      if (comment.body?.includes(prefixHeader)) {
+        core.info(`deleting comment ${comment.id}`);
+        octokit.rest.issues.deleteComment({
+          owner,
+          repo,
+          comment_id: comment.id
+        });
+      }
+    }
+
+    // Only post a new comment when there are changes
+    if (filteredDiffs.length) {
+      octokit.rest.issues.createComment({
+        issue_number: github.context.issue.number,
         owner,
         repo,
-        comment_id: comment.id
+        body: output
       });
     }
   }
 
-  // Only post a new comment when there are changes
-  if (filteredDiffs.length) {
-    octokit.rest.issues.createComment({
-      issue_number: github.context.issue.number,
+  async function getChangedFiles(): Promise<string[]> {
+    const { owner, repo } = github.context.repo;
+    const pull_number = github.context.issue.number;
+
+    const listFilesResponse = await octokit.rest.pulls.listFiles({
       owner,
       repo,
-      body: output
+      pull_number
+    });
+
+    return listFilesResponse.data.map(file => file.filename);
+  }
+
+  function partOfApp(changedFiles: string[], app: App): boolean {
+    const sourcePath = path.normalize(app.spec.source.path);
+    const appPath = getFirstTwoDirectories(sourcePath);
+
+    return changedFiles.some(file => {
+      const normalizedFilePath = path.normalize(file);
+      return normalizedFilePath.startsWith(appPath);
     });
   }
-}
 
-async function getChangedFiles(): Promise<string[]> {
-  const { owner, repo } = github.context.repo;
-  const pull_number = github.context.issue.number;
-
-  const listFilesResponse = await octokit.rest.pulls.listFiles({
-    owner,
-    repo,
-    pull_number
-  });
-
-  const changedFiles = listFilesResponse.data.map(file => file.filename);
-  return changedFiles;
-}
-
-function partOfApp(changedFiles: string[], app: App): boolean {
-  const sourcePath = path.normalize(app.spec.source.path);
-  const appPath = getFirstTwoDirectories(sourcePath);
-
-  return changedFiles.some(file => {
-    const normalizedFilePath = path.normalize(file);
-    return normalizedFilePath.startsWith(appPath);
-  });
-}
-
-function getFirstTwoDirectories(filePath: string): string {
-  const normalizedPath = path.normalize(filePath);
-  const parts = normalizedPath.split(path.sep).filter(Boolean); // filter(Boolean) removes empty strings
-  if (parts.length < 2) {
-    return parts.join(path.sep); // Return the entire path if less than two directories
+  function getFirstTwoDirectories(filePath: string): string {
+    const normalizedPath = path.normalize(filePath);
+    const parts = normalizedPath.split(path.sep).filter(Boolean); // filter(Boolean) removes empty strings
+    if (parts.length < 2) {
+      return parts.join(path.sep); // Return the entire path if less than two directories
+    }
+    return parts.slice(0, 2).join(path.sep);
   }
-  return parts.slice(0, 2).join(path.sep);
-}
 
-async function asyncForEach<T>(
-  array: T[],
-  callback: (item: T, i: number, arr: T[]) => Promise<void>
-): Promise<void> {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
+  async function asyncForEach<T>(
+    array: T[],
+    callback: (item: T, i: number, arr: T[]) => Promise<void>
+  ): Promise<void> {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
   }
-}
 
-async function run(): Promise<void> {
   const argocd = await setupArgoCDCommand();
   const apps = await getApps();
   core.info(`Found apps: ${apps.map(a => a.metadata.name).join(', ')}`);
@@ -304,26 +316,27 @@ function filterDiff(diffText: string): string {
   const sections = diffText.split(/(?=^===== )/m);
 
   const filteredSection = sections
-    .map(section => {
-      return section
+    .map(section =>
+      section
         .replace(
           /(\d+(,\d+)?c\d+(,\d+)?\n)?<\s+argocd\.argoproj\.io\/instance:.*\n---\n>\s+argocd\.argoproj\.io\/instance:.*\n?/g,
           ''
         )
         .trim()
         .replace(/(\d+(,\d+)?c\d+(,\d+)?\n)?<\s+app.kubernetes.io\/part-of:.*\n?/g, '')
-        .trim();
-    })
-    .filter(section => section.trim() !== '');
+        .trim()
+    )
+    .filter(section => section !== '');
 
-  const removeEmptyHeaders = filteredSection.filter(entry => {
-    // Remove empty strings and sections that are just headers with line numbers
-    return !entry.match(/^===== .*\/.* ======$/);
-  });
+  // Remove empty strings and sections that are just headers with line numbers
+  const removeEmptyHeaders = filteredSection.filter(entry => !entry.match(/^===== .*\/.* ======$/));
 
   // Join the filtered sections back together
   return removeEmptyHeaders.join('\n').trim();
 }
 
-// eslint-disable-next-line github/no-then
-run().catch(e => core.setFailed(e.message));
+// Avoid executing main automatically during tests
+if (require.main === module) {
+  // eslint-disable-next-line github/no-then
+  run().catch(e => core.setFailed(e.message));
+}
